5a472d90bd22821e16c6d77b43bad79d3336cd89
Seraphina Lee <seraphinalee@cayley3.math.cornell.edu>
2017-08-02 16:56:29 -0400

    thresh eigfns + weyl + counting for interval

--- thresheigfns/pdfconcat.py
+++ thresheigfns/pdfconcat.py
@@ -7,7 +7,7 @@ for m in ['1','2','3','4','5']:
         	pdfs = ''
         	index = 1
         	while os.path.isfile('./'+m+'/'+p+'/'+a+'/'+str(index)+'.pdf'):
-            	pdfs = pdfs + './'+m+'/'+p+'/'+a+'/'+str(index)+'.pdf ' 
-            	index = index + 1
-        	os.system('pdfunite ' + pdfs + 'output/m'+m+'p'+p+'.pdf')
+            		pdfs = pdfs + './'+m+'/'+p+'/'+a+'/'+str(index)+'.pdf ' 
+            		index = index + 1
+        		os.system('pdfunite ' + pdfs + 'output/m'+m+'p'+p+'cut'+a+'.pdf')
 
