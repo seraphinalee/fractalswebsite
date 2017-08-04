7c253f4cb79838986f802caa5c7bda0db793eac6
Seraphina Lee <el542@cornell.edu>
2017-08-04 15:33:08 -0400

    weyl and counting updated

--- counting/pdfconcat.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for p in ['10','20','30','40', '49', '50', '51','60','70','80', '90']:
-	pdfs = ''
-    	for a in ['1', '2', '3', '4', '5']:
-        	pdfs = pdfs +'./p'+p+'cut'+a+'.pdf '
-        	os.system('pdfunite ' + pdfs + 'output/p'+p+'.pdf')
-
